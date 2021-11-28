04c06233f5095e4300238b9834a65a908a602f19
ngochai0601 <91452618+ngochai0601@users.noreply.github.com>
2021-11-29 03:41:37 +0700

    submit

---
+++ TraCuuDiem_Web/TraCuuDiem/TraCuuDiem/Models/XemDiem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TraCuuDiem.Models
+{
+    public class XemDiem
+    {
+        public string hk { get; set; }
+        public string mamh { get; set; }
+        public string tenmh { get; set; }
+        public string tinchi { get; set; }
+        public string lopdukien { get; set; }
+        public string TL { get; set; }
+        public string GK { get; set; }
+        public string KT1 { get; set; }
+        public string KT2 { get; set; }
+        public string tongket { get; set; }
+        public string diem4 { get; set; }
+        public string diemchu { get; set; }
+        public string xeploai { get; set; }
+    }
+}
